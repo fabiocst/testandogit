2bfca31a1cf67a5a9f19451f82355e8ec7683d6a
fabio <fabio@DESKTOP-UMBUNC5>
2023-04-18 18:24:01 -0300

    Primeiro Commit

---
+++ Olá Mundo/src/olá/mundo/OláMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olá.mundo;
+
+/**
+ *
+ * @author fabio
+ */
+public class OláMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Testando o GitHub");
+        // TODO code application logic here
+    }
+    
+}
